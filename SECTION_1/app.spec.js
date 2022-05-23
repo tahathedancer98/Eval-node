c7b9d9155b55baa85b2d814d22de9ef72081f0ec
TahaTOUFIK <bookingtaha98@gmail.com>
2022-05-23 14:53:23 +0200

    L'ajout du test de la route /taches/id

--- SECTION_1/app.spec.js
+++ SECTION_1/app.spec.js
@@ -2,10 +2,15 @@ const request = require("supertest");
 const app = require("./app");
 
 describe("GET / ", () => {
-
+  // GET ALL TACHES TEST
   test("It should respond with an array of taches", async () => {
     const response = await request(app).get("/taches");
     expect(response.statusCode).toBe(200);
   });
+  // GET ONE TACHE TEST
+  test("It shoud get a specific tache", async () => {
+    const response = await request(app).get("/taches/1");
+    expect(response.statusCode).toBe(200);
+  })
   
 });
